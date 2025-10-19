54cd232fd4f1487d253d310b401cc484eb87964c
Ando <rakotoniainaandomalala@gmail.com>
2025-10-19 23:29:34 +0300

    Frame

---
+++ framework/src/main/java/com/framework/servlet/FrontServlet.java
@@ -0,0 +1,39 @@
+package com.framework.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class FrontServlet extends HttpServlet {
+
+    @Override
+    public void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String path = request.getRequestURI().substring(request.getContextPath().length());
+        boolean ressourceContext = getServletContext().getResource(path)!=null;
+        if(!ressourceContext){
+            response.getWriter().println(path);
+        }
+        else{
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/default");
+            dispatcher.forward(request, response);
+        }
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        service(request, response);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        service(request, response);
+    }
+}
