b2a3cb06ff3aac36839ad8d6b5261290cf34203b
Ando <rakotoniainaandomalala@gmail.com>
2025-10-19 23:33:02 +0300

    Update UrlController.java

--- framework/src/main/java/com/framework/annotation/UrlController.java
+++ framework/src/main/java/com/framework/annotation/UrlController.java
@@ -5,6 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+// avec annotation 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface UrlController{
